9ad72e0ce01e7b38ba30f7da94dc1eb75979a3ba
rikiwhyudi <rikiwhyudi45@gmail.com>
2023-10-12 11:39:46 +0700

    fix typo

--- repositories/rekening.go
+++ repositories/rekening.go
@@ -6,7 +6,7 @@ import (
 )
 
 type AccountNumberRepository interface {
-	GetBalanceRepository(ctx context.Context, ccountNumber int) (models.AccountNumber, error)
+	GetBalanceRepository(ctx context.Context, accountNumber int) (models.AccountNumber, error)
 	DepositRepository(ctx context.Context, deposit models.AccountNumber) (models.AccountNumber, error)
 	CashoutRepository(ctx context.Context, cashout models.AccountNumber) (models.AccountNumber, error)
 }
